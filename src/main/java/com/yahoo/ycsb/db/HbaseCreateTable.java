5dcf4baf9b0ff880d1029ae54c902dc21c8f3ef1
Ted Dunning <ted.dunning@gmail.com>
2011-02-01 14:58:50 -0800

    Utility for creating multi-region tables

---
+++ src/main/java/com/yahoo/ycsb/db/HbaseCreateTable.java
@@ -0,0 +1,8 @@
+package com.yahoo.ycsb.db;
+
+/**
+ * Created by IntelliJ IDEA. User: tdunning Date: 2/1/11 Time: 1:49 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class HbaseCreateTable {
+}
