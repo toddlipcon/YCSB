a7e9d594e86164cbd7c8cfefc5719d51741f5882
Ted Dunning <ted.dunning@gmail.com>
2011-05-02 14:03:58 -0700

    Added capability to abort computation.

--- src/main/java/com/yahoo/ycsb/RateLimiter.java
+++ src/main/java/com/yahoo/ycsb/RateLimiter.java
@@ -19,7 +19,16 @@ class RateLimiter<T extends ClientTask> implements Callable<Integer> {
   private double target;
   private ClientTask task;
   private AtomicBoolean done = new AtomicBoolean(false);
+  private AtomicBoolean shouldContinue = new AtomicBoolean(true);
 
+  /**
+   * A rate limiter object wil try to call the task object the specified number of times
+   * at the specified rate or until the cancel method is called.  A moving window of start
+   * times is kept so that we can keep the average rate tightly controlled.
+   * @param opCount               Desired number of operations except that opCount == 0 implies no end.
+   * @param targetOpsPerMs        Number of operations per millisecond, on average.
+   * @param task                  The task to call.
+   */
   public RateLimiter(double opCount, double targetOpsPerMs, ClientTask task) {
     this.opCount = opCount;
     this.target = targetOpsPerMs;
@@ -36,7 +45,7 @@ class RateLimiter<T extends ClientTask> implements Callable<Integer> {
 
     // we keep a moving window of start times to get better estimates
     Deque<Long> starts = new ArrayDeque<Long>(200);
-    while ((opCount == 0) || (opsDone < opCount)) {
+    while (shouldContinue.get() && ((opCount == 0) || (opsDone < opCount))) {
       // remember start time
       starts.add(System.currentTimeMillis());
 
@@ -68,12 +77,24 @@ class RateLimiter<T extends ClientTask> implements Callable<Integer> {
     return task.finish();
   }
 
+  /**
+   * Causes at most one more operation to be done.
+   */
+  public void cancel() {
+    shouldContinue.set(false);
+  }
 
-
+  /**
+   * Returns number of operations already completed.
+   * @return  The number of operations done.
+   */
   public int getOpsDone() {
     return task.getOpsDone();
   }
 
+  /**
+   * @return true if no more tasks are to be done.
+   */
   public boolean isFinished() {
     return done.get();
   }
